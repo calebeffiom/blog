fe5886cad724529ece5764434a0ac145098971d9
calebeffiom <calebeffiom974@gmail.com>
2025-03-08 17:47:22 +0100

    created the profile layout

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -2,5 +2,6 @@ import AuthLayout from "./auth_layout";
 import LandingLayout from "./landing_layout";
 import LatestStories from "./latest_stories";
 import TopStories from "./top_stories"
+import ProfileLayout from "./profile_layout";
 
-export {AuthLayout, LandingLayout, TopStories, LatestStories}
+export {AuthLayout, LandingLayout, TopStories, LatestStories, ProfileLayout}
---
+++ src/components/layouts/profile_layout.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../shared";
+import { Profile } from "../pages/profile";
+const ProfileLayout = ()=>{
+    return(<>
+    <Navbar/>
+    <Profile/>
+    </>)
+}
+export default ProfileLayout;
